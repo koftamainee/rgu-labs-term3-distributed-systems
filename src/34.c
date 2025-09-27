e0cefd6910cd59c102321a9e0fa4fc722fb6b9e2
koftamainee <koftamainee@gmail.com>
2025-09-27 22:49:19 +0300

    x_x

--- src/34.c
+++ src/34.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <wait.h>
 
 #define MAX_N 1024
 
@@ -13,6 +14,24 @@ typedef struct {
 } message_t;
 
 char *fifo_name(const char *base, int idx);
+
+void cleanup_fifos(char **paths, int n, const char *report, const char *ready) {
+  if (paths) {
+    for (int i = 0; i < n; ++i) {
+      if (paths[i]) {
+        unlink(paths[i]);
+        free(paths[i]);
+      }
+    }
+    free(paths);
+  }
+  if (report) {
+    unlink(report);
+  }
+  if (ready) {
+    unlink(ready);
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -80,28 +99,124 @@ int main(int argc, char *argv[]) {
       unlink(ready_fifo);
       perror("malloc ring");
       return 1;
-      perror("mkfifo ring");
     }
   }
 
   for (int i = 0; i < process_count; i++) {
     pid_t pid = fork();
     if (pid < 0) {
-      // TODO help
+      cleanup_fifos(fifo_paths, process_count, report_fifo, ready_fifo);
+      perror("fork");
       exit(1);
     }
 
     if (pid == 0) {
-      sleep(1);
-      int index = i;
-      int fd_in = open(fifo_paths[index], O_RDONLY);
+      int idx = i;
+      int fd_in = open(fifo_paths[idx], O_RDONLY | O_NONBLOCK);
       if (fd_in < 0) {
-        // TODO
+        perror("child open fd_in");
         exit(1);
+      }
+
+      printf("%d opened input pipe at %s\n", getpid(), fifo_paths[idx]);
+
+      if (idx == process_count - 1) {
+        int fd_ready = open(ready_fifo, O_WRONLY);
+        if (fd_ready >= 0) {
+          char ready_byte = 1;
+          write(fd_ready, &ready_byte, 1);
+          close(fd_ready);
+          printf("%d sent ready message to the parent\n", getpid());
+        }
+      }
+      printf("%d is ready.\n", getpid());
+      int fd_out = open(fifo_paths[(idx + 1) % process_count], O_WRONLY);
+      if (fd_out < 0) {
+        perror("open fd_out");
+        close(fd_in);
+        exit(1);
+      }
+      while (1) {
+        message_t msg;
+        int r = read(fd_in, &msg, sizeof(msg));
+        if (r != sizeof(msg)) {
+          continue;
+        }
+        printf("%d recivied message: %d\n", getpid(), msg.count);
+        // msg.count--;
+        if (msg.count == 0) {
+          printf("%d get count at 0. Exiting.\n", getpid());
+          close(fd_in);
+          close(fd_out);
+          exit(0);
+        }
+
+        int w = write(fd_out, &msg, sizeof(msg));
+        if (w != sizeof(msg)) {
+          perror("write fd_out");
+          close(fd_in);
+          close(fd_out);
+          exit(1);
+        }
+
+        printf("%d sent message to the next target. Exiting\n", getpid());
+
+        exit(0);
       }
     }
   }
+
+  int fd_ready = open(ready_fifo, O_RDONLY);
+  if (fd_ready < 0) {
+    cleanup_fifos(fifo_paths, process_count, report_fifo, ready_fifo);
+    perror("open fifo fd_ready write");
+    return 1;
+  }
+  int ready_flag = 0;
+  read(fd_ready, &ready_flag, 1);
+
+  /// main parent code
+
+  int fd_first_message = open(fifo_paths[0], O_WRONLY | O_NONBLOCK);
+  if (fd_first_message < 0) {
+    perror("open fifo for first message");
+
+    cleanup_fifos(fifo_paths, process_count, report_fifo, ready_fifo);
+    return 1;
+  }
+  printf("%d opened %s for first message\n", getpid(), fifo_paths[0]);
+
+  message_t msg;
+  msg.num_alive = process_count;
+  msg.count = step;
+  for (int i = 0; i < process_count; i++) {
+    msg.alive[i] = i;
+  }
+
+  int w = write(fd_first_message, &msg, sizeof(msg));
+  if (w != sizeof(msg)) {
+    perror("parent write first message");
+    cleanup_fifos(fifo_paths, process_count, report_fifo, ready_fifo);
+    return 1;
+  }
+  close(fd_first_message);
+
+  printf("Parent sent first message\n");
+
+  int status;
+  pid_t pid;
+  while ((pid = wait(&status)) > 0) {
+    if (WIFEXITED(status)) {
+      printf("Child %d exited with status %d\n", pid, WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+      printf("Child %d killed by signal %d\n", pid, WTERMSIG(status));
+    }
+  }
+
+  cleanup_fifos(fifo_paths, process_count, report_fifo, ready_fifo);
+  return 0;
 }
+
 char *fifo_name(const char *base, int idx) {
   char *s = malloc(256);
   if (s == NULL) {
